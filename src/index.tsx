d5201f8a3291064ed19dbe0f7ec240a4e61c16db
Olesia Opiakina <olesya.lo2017@yandex.ru>
2026-02-16 22:53:41 +0100

    full project

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
-
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import {
+	ArticleStateType,
+	defaultArticleState,
+} from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,19 +15,41 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [articleSettings, setArticleSettings] = useState(defaultArticleState);
+
+	function handleClick() {
+		setIsOpen((isOpen) => !isOpen);
+	}
+
+	function handleApply(currentSettings: ArticleStateType) {
+		setArticleSettings(currentSettings);
+		setIsOpen(false);
+	}
+
+	function handleClear() {
+		setArticleSettings(defaultArticleState);
+		setIsOpen(false);
+	}
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': articleSettings.fontFamilyOption.value,
+					'--font-size': articleSettings.fontSizeOption.value,
+					'--font-color': articleSettings.fontColor.value,
+					'--container-width': articleSettings.contentWidth.value,
+					'--bg-color': articleSettings.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				isOpen={isOpen}
+				onClick={handleClick}
+				onApply={handleApply}
+				onClear={handleClear}
+			/>
 			<Article />
 		</main>
 	);
